2c7c2ba2cd09f2f0dcc366539e2c932dce6c4040
Denise <giubileidenise@gmail.com>
2013-09-12 20:32:07 -0300

    add do appcontroller

---
+++ MC857IngrelizacaoDAC/src/com/br/unicamp/mc857integralizacaodac/utils/AppController.java
@@ -0,0 +1,89 @@
+package com.br.unicamp.mc857integralizacaodac.utils;
+
+import com.br.unicamp.mc857integralizacaodac.model.Atribuicao;
+import com.br.unicamp.mc857integralizacaodac.model.Catalogo;
+import com.br.unicamp.mc857integralizacaodac.model.Historico;
+
+public class AppController {
+	
+	private Historico historico;
+	private Catalogo catalogo;
+	private String ra;
+	private int curso;
+	
+	public AppController(String ra, int curso){
+		this.ra = ra;
+		this.curso = curso;
+	}
+		
+	public Boolean validarIntegralizacao(){
+		/* TODO: ­­ Método que retorna se é válida a integralização, dado um RA e uma integralização
+		require
+		o RA e a integralização devem ser válidos
+		ensure
+		a view será notificada com a resposta booleana do WebServiceMétodo que retorna se é válida a integralização, dado um RA e uma integralização*/
+		return true;
+	}
+	
+	public void recuperarInformacoesDoServico(){
+		
+		/* TODO: ­­ Método que preenche os atributos Catalogo e Historico vindo do WebService
+		require
+		o RA e o curso devem ser válidos
+		ensure
+		os atributos Catalogo e Historico foram preenchidos (não são nulos)*/
+		assert (!ra.isEmpty()) : "ra não pode ser nulo";
+		assert (curso > 0) : "curso inválido";
+		
+		String xmlHist=""; //TODO request trazendo  historico
+		String xmlCat=""; //TODO request trazendo catalogo
+		this.setHistorico(Parser.parseHistorico(xmlHist));
+		this.setCatalogo(Parser.parseCatalogo(xmlCat));
+		
+		assert (historico != null) : "historico inválido";
+		assert (catalogo != null) : "catalogo inválido";
+	}
+	
+	public Atribuicao gerarIntegralização(Historico historico, Catalogo catalogo) {
+		/* TODO: ­­ Método responsável por gerar a integralização a partir dos dados obtidos nos webservices
+		require
+		o histórico e o catálogo devem ser válidos
+		ensure
+		Todas as disciplinas do historico foram alocadas*/
+		Atribuicao atribuicao = new Atribuicao();
+		return atribuicao;
+	}
+
+	public Historico getHistorico() {
+		return historico;
+	}
+
+	public void setHistorico(Historico historico) {
+		this.historico = historico;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	
+	public int getCurso() {
+		return curso;
+	}
+
+	public void setCurso(int curso) {
+		this.curso = curso;
+	}
+
+	public Catalogo getCatalogo() {
+		return catalogo;
+	}
+
+	public void setCatalogo(Catalogo catalogo) {
+		this.catalogo = catalogo;
+	}
+	
+}
