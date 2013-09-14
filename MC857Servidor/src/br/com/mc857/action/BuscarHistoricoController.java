f49471168132092e9f65137dc7a416276dcd9619
davistuart <davi.stuart@gmail.com>
2013-09-14 09:10:18 -0700

    BuscarHistoricoController.java precisa testar README com informacoes pertinentes

--- MC857Servidor/src/br/com/mc857/action/BuscarHistoricoController.java
+++ MC857Servidor/src/br/com/mc857/action/BuscarHistoricoController.java
@@ -1,7 +1,12 @@
 package br.com.mc857.action;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringBufferInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
@@ -9,20 +14,48 @@ import org.apache.struts2.convention.annotation.Result;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BuscarHistoricoController  extends ActionSupport{
-	
+
+	private final String URL_BASE = "http://localhost:81//";
+
 	private String ra;
-	
-	  private InputStream inputStream;
-	  public InputStream getInputStream() {
-	    return inputStream;
-	   }
-	
+
+	private InputStream inputStream;
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
 	@Action(value = "buscaHistorico", results = {
 			@Result( type="stream", name=SUCCESS)}
-	)
-	public String buscaHistorico(){
-	    inputStream = new StringBufferInputStream(
-	    	      "aqui vem a treta pro ra "+ ra);
+			)
+	public String buscaHistorico() {
+		String url = "?serv=h&ra="+ra;
+		StringBuffer response;
+
+		try {
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			con.setRequestMethod("GET");
+
+			con.setRequestProperty("User-Agent", "");
+
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+
+			return ERROR;
+		}
+
+		inputStream = new StringBufferInputStream(
+				response.toString());
 		return SUCCESS;
 	}
 
--- README.md
+++ README.md
@@ -18,3 +18,28 @@ Etapas
 [ ] Testes
 >
 [ ] Entrega Final
+
+Instalando Python
+=====
+>
+Faça o download do Python 2.7 em: http://www.python.org/ftp/python/2.7.5/python-2.7.5.msi (32-bits) ou http://www.python.org/ftp/python/2.7.5/python-2.7.5.amd64.msi (64-bits)
+>
+No passo que ele pergunta se quer salvar para todos usuários ou só o seu, escolha apenas o seu
+
+Rodando o servidor
+=====
+>
+Faça o download do .rar do servidor do professor: http://www.ic.unicamp.br/~vanini/mc857/servidorDeDados.zip
+>
+Abra um terminal (como adm no windows) e navegue até a pasta do servidor
+>
+Execute: "Python servidorDeDados.py", caso fale que Python não é um comando execute o Python diretamente (C:\Python2.7\python.exe servidorDeDadps.py) ou configure as variáveis de ambiente.
+>
+Caso ele dê outro erro 10030, ou algo do tipo, é a porta 80 do seu computador já sendo usada, edite o arquivo servidorDeDados.py e coloque 81, ou alguma superior até funcionar. Quando funcionar uma tela abrirá pedindo para liberar o acesso,.
+
+Exemplo de uso
+=====
+>
+Para usar, precisamos passar qual tipo de serviço queremos, eles estão listados no dados.py
+>
+Exemplo: "http://localhost:81//?serv=c&cod=34"
