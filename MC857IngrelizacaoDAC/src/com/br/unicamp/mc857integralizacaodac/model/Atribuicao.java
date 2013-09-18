bc2490849cd1aea9c5533386ddc4c833bca3f914
Lucas <luksfarris@gmail.com>
2013-09-17 22:25:57 -0300

    Update Atribuicao.java
    adding boolean value to tell if the attribution is valid

--- MC857IngrelizacaoDAC/src/com/br/unicamp/mc857integralizacaodac/model/Atribuicao.java
+++ MC857IngrelizacaoDAC/src/com/br/unicamp/mc857integralizacaodac/model/Atribuicao.java
@@ -1,5 +1,14 @@
 package com.br.unicamp.mc857integralizacaodac.model;
 
 public class Atribuicao extends Catalogo {
+	/** diz se a atribuicao corresponde a uma integralizacao completa */
+	private boolean integral;
 
+	public boolean isIntegral() {
+		return integral;
+	}
+
+	public void setIntegral(boolean integral) {
+		this.integral = integral;
+	}
 }
