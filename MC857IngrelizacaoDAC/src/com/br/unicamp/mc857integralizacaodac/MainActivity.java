48343d107b48433f4514b86c69fa4e7ba5c3b0df
Lucas Farris <luksfarris@gmail.com>
2013-10-29 22:07:08 -0200

    missing imports

--- MC857IngrelizacaoDAC/src/com/br/unicamp/mc857integralizacaodac/MainActivity.java
+++ MC857IngrelizacaoDAC/src/com/br/unicamp/mc857integralizacaodac/MainActivity.java
@@ -2,12 +2,14 @@ package com.br.unicamp.mc857integralizacaodac;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.br.unicamp.mc857ingrelizacaodac.R;
